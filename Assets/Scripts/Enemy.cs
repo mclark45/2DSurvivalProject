23d09a18eac293b06c37da0dd64398159a8c62a2
mclark45 <80595429+mclark45@users.noreply.github.com>
2023-07-11 20:27:46 -0400

    adding colliders and enemy abstract class

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,31 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public abstract class Enemy : MonoBehaviour
 {
-    private GameObject _player;
-    [SerializeField] private float _speed = 1.0f;
-    void Start()
+    protected GameObject _player;
+    [SerializeField] protected float _speed = 1.0f;
+
+    public virtual void Init()
     {
-        if (GameObject.FindWithTag("Player") != null)
-        {
-            _player = GameObject.FindWithTag("Player");
-        }
-        else
-        {
-            Debug.LogError("Player was not found!!!");
-        }
+        SetTarget();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
+        // Sets the enemies target to the player.
+        Init();
+    }
+
+    protected virtual void Update()
+    {
+        // Updates the enemies position to chase player.
         Chase();
     }
 
-    private void Chase()
+    protected virtual void SetTarget()
     {
+        // Set the player gameobject if the player is found or give error that player is not found.
+        if (GameObject.FindWithTag("Player") != null)
+            _player = GameObject.FindWithTag("Player");
+
+        else
+            Debug.LogError("Player was not found!!!");
+    }
+
+    protected virtual void Chase()
+    {
+       // Sets the enemies forward position to face the player.
         transform.right = _player.transform.position - transform.position;
+        // Updates the enemies position to move toward the player.
         transform.position += transform.right * _speed * Time.deltaTime;
     }
 }
